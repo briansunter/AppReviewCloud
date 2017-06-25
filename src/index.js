5f6442537db77d978990b4cab82bbfc37ef9cf90
Brian Sunter <bsunter@gmail.com>
2017-06-24 17:44:18 -0700

    Redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import WordCloud from 'react-d3-cloud';
 import {ofType, delay, MapTo} from 'rxjs'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createEpicMiddleware } from 'redux-observable';
+import { connect } from 'react-redux';
 
 const data = [
   { text: 'Hey', value: 1000 },
@@ -10,34 +14,6 @@ const data = [
   { text: 'very cool', value: 1000000 },
   { text: 'duck', value: 10 },
 ];
-
-const fontSizeMapper = word => word.value / 20;
-const rotate = word => (word.value % 90) - 45;
-
-class WordCloudDemo extends React.Component {
-  render() {
-    const newData = data.map(item => ({
-      text: item.text,
-      value: Math.random() * 1000,
-    }));
-    return (
-      <WordCloud
-        width={1000}
-        height={750}
-        data={newData}
-        fontSizeMapper={fontSizeMapper}
-        rotate={rotate}
-        padding={2}
-      />
-    );
-  }
-}
-
-/* ReactDOM.render(
- *   <WordCloudDemo />,
- *   document.getElementById('root')
- * );*/
-
 
 const PING = 'PING';
 const PONG = 'PONG';
@@ -49,7 +25,12 @@ const pingEpic = action$ =>
            .delay(1000) // Asynchronously wait 1000ms then continue
            .mapTo({ type: PONG });
 
-const pingReducer = (state = { isPinging: false }, action) => {
+const defaultState = {
+    isPinging: false,
+    reviews: data,
+};
+
+const pingReducer = (state = defaultState, action) => {
     switch (action.type) {
         case PING:
             return { isPinging: true };
@@ -62,35 +43,40 @@ const pingReducer = (state = { isPinging: false }, action) => {
     }
 };
 
-// components/App.js
-
-import { connect } from 'react-redux';
-
-let App = ({ isPinging, ping }) => (
-    <div>
-        <h1>is pinging: {isPinging.toString()}</h1>
-        <button onClick={ping}>Start PING</button>
-    </div>
-);
-
-App = connect(
-    ({ isPinging }) => ({ isPinging }),
-    { ping }
-)(App);
-
-// redux/configureStore.js
-
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { createEpicMiddleware } from 'redux-observable';
-
 const epicMiddleware = createEpicMiddleware(pingEpic);
 
 const store = createStore(pingReducer,
                           applyMiddleware(epicMiddleware)
 );
 
+const fontSizeMapper = word => word.value / 20;
+const rotate = word => (word.value % 90) - 45;
+
+/* ReactDOM.render(
+ *   <WordCloudDemo />,
+ *   document.getElementById('root')
+ * );*/
+
 // index.js
+
+let App = ({ isPinging, ping, reviews}) => (
+    <div>
+        <h1>is pinging: {isPinging.toString()}</h1>
+        <button onClick={ping}>Start PING</button>
+        <WordCloud
+            width={1000}
+            height={750}
+            data={reviews}
+            fontSizeMapper={fontSizeMapper}
+            rotate={rotate}
+            padding={2}/>
+    </div>
+);
+
+App = connect(
+    ({ isPinging, reviews }) => ({ isPinging, reviews }),
+    { ping }
+)(App);
 
 ReactDOM.render(
     <Provider store={store}>
